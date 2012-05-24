df60d0793710c60919dd1707f6cadf1ed81ecc45
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-05-24 19:50:28 +0200

    Added skeleton files for some planned functions.

---
+++ ImageAnalysis/ImageClip.m
@@ -0,0 +1,51 @@
+function out = ImageClip( in )
+%% Short description.
+%
+% out = ImageClip( in )
+%
+% Input parameters (required):
+%
+%
+%
+% Input parameters (optional):
+%
+%
+%
+% Output parameters:
+%
+%
+%
+% Description:
+%
+%
+%
+% Example:
+%
+%
+%
+% See also
+
+% Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+%
+% This program is free software; you can redistribute it and/or modify it under
+% the terms of the GNU General Public License as published by the Free Software
+% Foundation; either version 3 of the License, or (at your option) any later
+% version.
+%
+% This program is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+% details.
+%
+% You should have received a copy of the GNU General Public License along with
+% this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+% Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+% Last revision on: 24.05.2012 19:47
+
+%% Check Input and Output Arguments
+
+%% Algorithm
+
+
+end
