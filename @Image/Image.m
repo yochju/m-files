c4b8a002cf7b6c9d6f67a451fd3cf986f3a922f0
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-07-01 22:23:50 +0200

    Added image dimensions and pixeltype as properties.

--- @Image/Image.m
+++ @Image/Image.m
@@ -2,13 +2,23 @@ classdef Image
     %Image Represents an image in form of an n-D array.
 
     properties
-        data
+        data = [];
+    end
+    
+    properties (GetAccess = public, SetAccess = private)
+        rows = 0;
+        cols = 0;
+        chan = 0;
+    end
+    
+    properties (Dependent = true, SetAccess = private)
+        pixelType
     end
         
     methods
         function obj = Image(rows,columns,varargin)
             
-            error(nargchk(2, 3, nargin));
+            error(nargchk(3, 4, nargin));
             error(nargoutchk(0, 1, nargout));
 
             parser = inputParser;
@@ -18,16 +28,25 @@ classdef Image
                 {'numeric'}, ...
                 {'integer','nonnegative','scalar'}, ...
                 'Image', 'rows'));
+            
             parser.addRequired('columns', ...
                 @(x) validateattributes( x, ...
                 {'numeric'}, ...
                 {'integer','nonnegative','scalar'}, ...
                 'Image', 'columns'));
+            
+            parser.addOptional('channels', 1, ...
+                @(x) validateattributes( x, ...
+                {'numeric'}, ...
+                {'integer','positive','scalar'}, ...
+                'Image', 'channels'));
+            
             parser.addOptional('type','double', ...
                 @(x) validateattributes( x, ...
                 {'char'}, ...
                 {'vector'}, ...
                 'Image', 'type'));
+            
             parser.parse(rows,columns,varargin{:});
             parameters = parser.Results;
             
@@ -42,16 +61,31 @@ classdef Image
                     parameters.type, ...
                     '. Will use double as fallback.'));
                 warning(ExcM.id,ExcM.message);
-                obj.data = zeros( ...
-                    parameters.rows, parameters.columns, 'double');
-                
-                return
+                parameters.type = 'double';
             end
             
-            obj.data = zeros( ...
-                parameters.rows, parameters.columns, parameters.type);
+            obj.rows = parameters.rows;
+            obj.cols = parameters.columns;
+            obj.chan = parameters.channels;
+            
+            obj.data = squeeze(zeros( ...
+                parameters.rows, parameters.columns, ...
+                parameters.channels, parameters.type));
         end
         
+        function pType = get.pixelType(obj)
+            pType = class(obj.data);
+        end
+
+    end
+    
+
+    methods
+        [rows cols channels] = size(obj)
+    end
+    
+    methods (Access = private)
+        bool = compatible(obj1,obj2)
     end
     
 end
