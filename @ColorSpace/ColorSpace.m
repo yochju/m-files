ff3c5f696e862ccfbfcdac52b94896d0f83020c8
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-07-15 22:01:36 +0200

    Added color space class for usage with image class.

---
+++ @ColorSpace/ColorSpace.m
@@ -0,0 +1,13 @@
+classdef ColorSpace < uint8
+    %ColorSpace Simple enumeration class for color spaces.
+    
+    enumeration
+        Unknown (0)
+        Gray    (1)
+        RGB     (2)
+        HSV     (3)
+        YCbCr   (4)
+    end
+    
+end
+
