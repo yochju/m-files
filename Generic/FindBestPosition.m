933a1e4480d32af1305e52f45a1762fe217ca3cc
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-09-05 17:47:14 +0200

    Improved input checking and error handling.

--- Generic/FindBestPosition.m
+++ Generic/FindBestPosition.m
@@ -1,16 +1,41 @@
-function [ p ] = FindBestPosition(V,x,pos)
-% [ p ] = FindBestPosition(V,x,pos)
+function p = FindBestPosition(V, x, varargin)
+%% Find closest match inside a given vector.
+%
+% p = FindBestPosition(V, x, ...)
+%
+% Input parameters (required):
+%
+% V : vector to be searched (array)
+% x : value to be found. (scalar)
+%
+% Input parameters (optional):
+%
+% pos : either 'first' or 'last'. (case insensitive, default = 'first')
+%
+% Output paramaters:
+%
+% p : position of the best approximation to x inside V. (scalar)
+%
+% Description:
+%
 % Looks for the best approximation of the scalar x inside the vector V and
 % returns by default the position of the first occurence. This behaviour can
-% be overriden by providing a thrid parameter which may be either 'first' or
+% be overriden by providing a third parameter which may be either 'first' or
 % 'last' to return the respective occurence.
 %
-% Usage: [ p ] = FindBestPosition(V,x,pos)
-% V   : the search space.
-% x   : the value to be found.
-% pos : if 'first' the first occurence is returned. If 'last' it returns the
-%       last occurence.
-% p   : the position of the best approximation to x.
+% Example:
+%
+% V = [ 0 0 1 2 2 3 4 5 ];
+% x = 2.1;
+% p = FindBestPosition(V,x,'first')
+%
+% yields p = 4;
+%
+% p = FindBestPosition(V,x,'last')
+%
+% yields p = 5;
+%
+% See also FindBestPositionq, interp1.
 
 % Copyright 2011,2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
 %
@@ -28,17 +53,30 @@ function [ p ] = FindBestPosition(V,x,pos)
 % this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 % Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-% Last revision: 05.09.2012 16:10
+% Last revision: 05.09.2012 17:02
 
-%% Check number of input and output arguments.
+%% Check Input and Output Arguments
 
 narginchk(2,3);
 nargoutchk(0,1);
 
-if nargin == 2
-    pos = 'first';
-end
-temp = unique(V);
-p = interp1(temp,1:length(temp) , x , 'nearest','extrap');
-p = find( V==temp(p) , 1 , pos );
+parser = inputParser;
+parser.FunctionName = mfilename;
+parser.CaseSensitive = false;
+parser.KeepUnmatched = true;
+parser.StructExpand = true;
+
+parser.addRequired('V', @(x) ismatrix(x));
+parser.addRequired('x', @(x) isscalar(x));
+parser.addOptional('pos','first',@(x) any(strcmpi(x,{'first','last'})));
+
+parser.parse(V,x,varargin{:});
+opts = parser.Results;
+
+% Use nearest neighbor interpolation to find best approximation inside V.
+temp = unique(opts.V);
+p = interp1(temp,1:length(temp) , opts.x , 'nearest','extrap');
+% Extract position of best approximation.
+p = find( opts.V==temp(p) , 1 , lower(opts.pos) );
+
 end
--- Generic/FindBestPositionq.m
+++ Generic/FindBestPositionq.m
@@ -1,17 +1,46 @@
-function p = FindBestPositionq(V,x,pos)
-% [ p ] = FindBestPositionq(V,x,pos)
-% Looks for the best approximation of the scalar x inside the vector V and
-% returns by default the position of the first occurence. This behaviour can
-% be overriden by providing a thrid parameter which may be either 'first' or
-% 'last' to return the respective occurence. This method is faster then
-% FindBestPosition, however it does not perform any error checking.
+function p = FindBestPositionq(V, x, varargin)
+%% Find closest match inside a given vector.
 %
-% Usage: [ p ] = FindBestPositionq(V,x,pos)
-% V   : the search space.
-% x   : the value to be found.
-% pos : if 'first' the first occurence is returned. If 'last' it returns the
-%       last occurence.
-% p   : the position of the best approximation to x.
+% p = FindBestPositionq(V, x, ...)
+%
+% Input parameters (required):
+%
+% V : vector to be searched (array)
+% x : value to be found. (scalar)
+%
+% Input parameters (optional):
+%
+% pos : either 'first' or 'last'. (case insensitive, default = 'first')
+%
+% Output paramaters:
+%
+% p : position of the best approximation to x inside V. (scalar)
+%
+% Description:
+%
+% Looks for the best approximation of the scalar x inside the vector V and
+% returns by default the position of the first occurence. This behaviour can be
+% overriden by providing a third parameter which may be either 'first' or 'last'
+% to return the respective occurence. This version is faster than
+% FindBestPosition. However, no error checking is being performed for the
+% interpolation step. Furthermore, the behaviour of the two methods is not
+% identical for values of x outside of the convex hull of V. FindBestPositionq
+% ignores the third parameter when extrapolation is being done. This method is
+% not recommended. Use with care.
+%
+% Example:
+%
+% V = [ 0 0 1 2 2 3 4 5 ];
+% x = 2.1;
+% p = FindBestPositionq(V,x,'first')
+%
+% yields p = 4;
+%
+% p = FindBestPositionq(V,x,'last')
+%
+% yields p = 5;
+%
+% See also FindBestPosition, interp1q.
 
 % Copyright 2011,2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
 %
@@ -36,17 +65,28 @@ function p = FindBestPositionq(V,x,pos)
 narginchk(2,3);
 nargoutchk(0,1);
 
-if nargin == 2
-    pos = 'first';
-end
+parser = inputParser;
+parser.FunctionName = mfilename;
+parser.CaseSensitive = false;
+parser.KeepUnmatched = true;
+parser.StructExpand = true;
 
-if x >= max(V)
-    p = length(V);
-elseif x <= min(V)
+parser.addRequired('V', @(x) ismatrix(x));
+parser.addRequired('x', @(x) isscalar(x));
+parser.addOptional('pos','first',@(x) any(strcmpi(x,{'first','last'})));
+
+parser.parse(V,x,varargin{:});
+opts = parser.Results;
+
+% Note the the extrapolation case is being handled differently than for
+% FindBestPosition.
+if x >= max(opts.V)
+    p = length(opts.V);
+elseif x <= min(opts.V)
     p = 1;
 else
-    temp = unique(V);
-    p = round(interp1q(temp, [1:length(temp)]' , x ));
-    p = find( V==temp(p) , 1 , pos );
+    temp = unique(opts.V);
+    p = round(interp1q(temp, (1:length(temp))' , opts.x ));
+    p = find( V==temp(p) , 1 , lower(opts.pos) );
 end
 end
