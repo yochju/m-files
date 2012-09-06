b26c3fc71b4bb83dd55e39734522f881f1e9b3b5
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-09-06 15:05:53 +0200

    General improvements across the whole function.

--- Generic/Threshold.m
+++ Generic/Threshold.m
@@ -1,8 +1,33 @@
-function  y  = Threshold( x , t )
+function  [y varargout]  = Threshold( x , t )
 %% Thresholds input x with respect to t.
 %
-% y  = Threshold( x , t )
+% [y, p, v]  = Threshold( x , t )
 %
+% Input parameters (required):
+%
+% x : vector to apply the thresholding on (array).
+% t : threshold (scalar).
+%
+% Output parameters:
+%
+% y : vector of same size as x, containing 1 if the entry was strictly larger
+%     than t or 0 else.
+% p : the positions of the indices that exceed t.
+% v : the values found at positions p.
+%
+% Description:
+%
+% Finds all values in x that are smaller or eequal in absolute value than t.
+%
+% Example:
+%
+% x = [-3 -1 0 1 2 ]
+% y = Threshold(x,1.5)
+%
+% yields y = [1 0 0 0 1].
+%
+% See also max.
+
 % Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
 %
 % This program is free software; you can redistribute it and/or modify it under
@@ -19,15 +44,42 @@ function  y  = Threshold( x , t )
 % this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 % Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-% Last revision: 2012/03/14 17:10
+% Last revision: 2012/09/06 14:10
 
 %% Check input parameters
 
-error(nargchk(2, 2, nargin));
-error(nargoutchk(0, 1, nargout));
+narginchk(2, 2);
+nargoutchk(0, 3);
+
+parser = inputParser;
+parser.FunctionName = mfilename;
+parser.CaseSensitive = false;
+parser.KeepUnmatched = true;
+parser.StructExpand = true;
+
+parser.addRequired('x', @(x) ismatrix(x));
+parser.addRequired('t', @(x) isscalar(x));
+
+parser.parse(x,t);
+opts = parser.Results;
+
+ExcM = ExceptionMessage('BadArg', '', ...
+    'Negative thresholding value encountered.');
+
+% TODO: The id is not recognised. Because it contains no :? See doc.
+%warning(ExcM.id,ExcM.message);
+warning(ExcM.message);
 
 %% Compute thresholding
 
-y = sign( max( abs(x) - t , 0 ) );
+y = sign( max( abs(opts.x) - opts.t , 0 ) );
+
+if nargout >= 2
+    varargout{1} = find(y);
+end
+
+if nargout >= 3
+    varargout{2} = opts.x(y==1);
+end
 
 end
