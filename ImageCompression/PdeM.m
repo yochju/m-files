bba7a589f2c97728427d337f937ef9340b717c67
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-09-24 16:23:06 +0200

    Thresholding the interpolation mask is now possible.

--- ImageCompression/PdeM.m
+++ ImageCompression/PdeM.m
@@ -14,6 +14,19 @@ function out = PdeM(c,varargin)
 % corresponding values or option/value pairs, where the option is specified as a
 % string.
 %
+% threshData : threshold that should be applied to the mask in front of the data
+%              term (u-f). (scalar, default = nan, e.g. no thresholding).
+% thrDataMin : value at which the mask for the data term should be set for
+%              values below the threshold. (scalar, default = 0).
+% thrDataMax : value at which the mask for the data term should be set for
+%              values above the threshold. (scalar, default = 1).
+% threshDiff : threshold that should be applied to the mask in front of the diff
+%              term Laplace u. (scalar, default = nan, e.g. no thresholding).
+% thrDiffMin : value at which the mask for the diff term should be set for
+%              values below the threshold. (scalar, default = 0).
+% thrDiffMax : value at which the mask for the diff term should be set for
+%              values above the threshold. (scalar, default = 1).
+%
 % Output parameters:
 %
 % out : Matrix correspinding to the left hand side of the discretised PDE
@@ -33,7 +46,10 @@ function out = PdeM(c,varargin)
 % with Neumann Boundary conditions. If c contains arbitrary values between 0 and
 % 1, the above model can be considered to be a fuzzy mixed boundary value
 % problem where c acts as the fuzzy indicator. Values for c outside of the
-% interval [0,1] are allowed. Their interpretation is left to the user.
+% interval [0,1] are allowed. Their interpretation is left to the user. The mask
+% for the interpolation sites can be binarized independently for the data term
+% and for the differential term. The binarization is being done with the
+% Binarize function.
 %
 % Example:
 % c = double(rand(100,100) > 0.6);
@@ -57,9 +73,9 @@ function out = PdeM(c,varargin)
 % this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 % Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-% Last revision on: 10.09.2012 11:55
+% Last revision on: 24.09.2012 12:28
 
-narginchk(1, 2);
+narginchk(1, 13);
 nargoutchk(0, 1);
 
 parser = inputParser;
@@ -70,6 +86,13 @@ parser.StructExpand = true;
 
 parser.addRequired('c', @(x) ismatrix(x)&&IsDouble(x));
 
+parser.addParamValue('threshData', nan, @(x) isscalar(x)&&IsDouble(x));
+parser.addParamValue('thrDataMin',  0,  @(x) isscalar(x)&&IsDouble(x));
+parser.addParamValue('thrDataMax',  1,  @(x) isscalar(x)&&IsDouble(x));
+parser.addParamValue('threshDiff', nan, @(x) isscalar(x)&&IsDouble(x));
+parser.addParamValue('thrDiffMin',  0,  @(x) isscalar(x)&&IsDouble(x));
+parser.addParamValue('thrDiffMax',  1,  @(x) isscalar(x)&&IsDouble(x));
+
 parser.parse(c, varargin{:})
 opts = parser.Results;
 
@@ -78,6 +101,11 @@ D = LaplaceM(row, col, ...
     'KnotsR',[-1 0 1],'KnotsC',[-1 0 1], ...
     'Boundary', 'Neumann');
 
-out = Mask(opts.c(:)) - (speye(length(opts.c(:))) - Mask(opts.c(:)))*D;
+cData = Binarize(opts.c(:), opts.threshData, ...
+    'min', opts.thrDataMin, 'max', opts.thrDataMax);
+cDiff = Binarize(opts.c(:), opts.threshDiff, ...
+    'min', opts.thrDiffMin, 'max', opts.thrDiffMax);
+
+out = Mask(cData) - (speye(length(opts.c(:))) - Mask(cDiff))*D;
 
 end
--- ImageCompression/SolvePde.m
+++ ImageCompression/SolvePde.m
@@ -1,41 +1,119 @@
-function out = SolvePde(f,c,s)
-    %Solves the Laplace equation with mixed boundary conditions.
-    
-    % Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
-    %
-    % This program is free software; you can redistribute it and/or modify it
-    % under the terms of the GNU General Public License as published by the Free
-    % Software Foundation; either version 3 of the License, or (at your option)
-    % any later version.
-    %
-    % This program is distributed in the hope that it will be useful, but
-    % WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-    % or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-    % for more details.
-    %
-    % You should have received a copy of the GNU General Public License along
-    % with this program; if not, write to the Free Software Foundation, Inc., 51
-    % Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-    
-    % Last revision on: 16.08.2012 17:30
-    
-    narginchk(2, 3);
-    nargoutchk(0, 1);
-    
-    tol   = 1e-8;
-    maxit = 20000;
-    
-    if nargin == 2
-        [out flag relres iter] = lsqr(PdeM(c),Rhs(c,f),tol,maxit);
-    elseif nargin == 3
-        [out flag relres iter] = lsqr(PdeM(c,s),Rhs(c,f),tol,maxit);
-    else
-        error('OPTCONT:BadP','Wrong number of parameters passed.');
-    end
-    
-    if flag ~= 0
-        warning('OPTCONT:Err', ...
-            'SolvePde failed with flag: %g, relative residual %g at iteration %d\n', flag, relres, iter);
-    end
-    
+function out = SolvePde(f, c, varargin)
+%% Solves the Laplace equation with mixed boundary conditions.
+%
+% out = SolvePde(f, c, ...)
+%
+% Input parameters (required):
+%
+% f : image data for the data sites. (double array)
+% c : mask indicating the positions where the dirichlet data should be applied.
+%     (double array)
+%
+% Input parameters (optional):
+%
+% Optional parameters are either struct with the following fields and
+% corresponding values or option/value pairs, where the option is specified as a
+% string.
+%
+% threshData : threshold that should be applied to the mask in front of the data
+%              term (u-f). (scalar, default = nan, e.g. no thresholding).
+% thrDataMin : value at which the mask for the data term should be set for
+%              values below the threshold. (scalar, default = 0).
+% thrDataMax : value at which the mask for the data term should be set for
+%              values above the threshold. (scalar, default = 1).
+% threshDiff : threshold that should be applied to the mask in front of the diff
+%              term Laplace u. (scalar, default = nan, e.g. no thresholding).
+% thrDiffMin : value at which the mask for the diff term should be set for
+%              values below the threshold. (scalar, default = 0).
+% thrDiffMax : value at which the mask for the diff term should be set for
+%              values above the threshold. (scalar, default = 1).
+% lsqrTol    : tolerance limit for the least squares solver. (scalar,
+%              default = 1e-8)
+% lsqrMaxit  : maximal number of iterations the least squares solver should do.
+%              (scalar, default = 20000)
+%
+% Output parameters:
+%
+% out : Solution of the considered PDE.
+%
+% Description:
+%
+% Solves the following PDE:
+%
+% c.*(u-f) - (1-c).*(u_xx + u_yy) = 0
+%
+% which corresponds to the Laplace equation with mixed (Robin-) boundary
+% conditions in case of a binary valued c if it is solved with a righthand side
+% of 0. Positions where c == 1, will represent Dirichlet boundary conditions and
+% positions where c == 0, will be the domain where the PDE should be solved.
+% Note that the outer boundaries (unless specified through c) will be modelled
+% with Neumann Boundary conditions. If c contains arbitrary values between 0 and
+% 1, the above model can be considered to be a fuzzy mixed boundary value
+% problem where c acts as the fuzzy indicator. Values for c outside of the
+% interval [0,1] are allowed. Their interpretation is left to the user. The mask
+% for the interpolation sites can be binarized independently for the data term
+% and for the differential term. The binarization is being done with the
+% Binarize function.
+%
+% Example:
+% c = double(rand(100,100) > 0.6);
+% f = rand(100,100);
+% SolvePde(f, c);
+%
+% See also EvalPde, PdeM
+
+% Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+%
+% This program is free software; you can redistribute it and/or modify it under
+% the terms of the GNU General Public License as published by the Free Software
+% Foundation; either version 3 of the License, or (at your option) any later
+% version.
+%
+% This program is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+% details.
+%
+% You should have received a copy of the GNU General Public License along with
+% this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+% Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+% Last revision on: 24.09.2012 12:28
+
+narginchk(2, 18);
+nargoutchk(0, 1);
+
+parser = inputParser;
+parser.FunctionName = mfilename;
+parser.CaseSensitive = false;
+parser.KeepUnmatched = true;
+parser.StructExpand = true;
+
+parser.addRequired('f', @(x) ismatrix(x)&&IsDouble(x));
+parser.addRequired('c', @(x) ismatrix(x)&&IsDouble(x));
+
+parser.addParamValue('threshData', nan, @(x) isscalar(x)&&IsDouble(x));
+parser.addParamValue('thrDataMin',  0,  @(x) isscalar(x)&&IsDouble(x));
+parser.addParamValue('thrDataMax',  1,  @(x) isscalar(x)&&IsDouble(x));
+parser.addParamValue('threshDiff', nan, @(x) isscalar(x)&&IsDouble(x));
+parser.addParamValue('thrDiffMin',  0,  @(x) isscalar(x)&&IsDouble(x));
+parser.addParamValue('thrDiffMax',  1,  @(x) isscalar(x)&&IsDouble(x));
+
+parser.addParamValue('lsqrTol',        1e-8, @(x) isscalar(x)&&IsDouble(x));
+parser.addParamValue('lsqrMaxit',  20000,    @(x) isscalar(x)&&IsDouble(x));
+
+parser.parse(f, c, varargin{:})
+opts = parser.Results;
+
+[out flag relres iter] = lsqr( ...
+    PdeM(opts.c, opts), ...
+    Rhs(c,f), ...
+    opts.lsqrTol, opts.lsqrMaxit, ...
+    speye(numel(f), numel(f)), speye(numel(f), numel(f)), f);
+
+if flag ~= 0
+    warning('OPTCONT:Err', ...
+        'SolvePde failed with flag: %g, relative residual %g at iteration %d\n', flag, relres, iter);
+end
+
 end
