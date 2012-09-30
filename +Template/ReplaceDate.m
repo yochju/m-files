2af248b0ab71709b75615fc7994fa345bb47b5cd
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-09-30 15:06:24 +0200

    Added a small comment as a ToDo note.

--- +Template/ReplaceDate.m
+++ +Template/ReplaceDate.m
@@ -1,4 +1,4 @@
 function out = ReplaceDate(in,t)
-    % default t = now.
+    % default t = now. (should not remove the \$. makes updating easier.
     out = regexprep(in, '\$DATE\$', datestr(t,31), 'ignorecase');
 end
