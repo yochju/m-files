d49539da114fe3e04a794e6f085ac20fcf9185cf
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-10-10 07:48:41 +0200

    Updated License information.

--- @ParseInput/CheckNumOutput.m
+++ @ParseInput/CheckNumOutput.m
@@ -1,4 +1,23 @@
 function CheckNumOutput(obj, numArgs)
+
+% Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+%
+% This program is free software; you can redistribute it and/or modify it under
+% the terms of the GNU General Public License as published by the Free Software
+% Foundation; either version 3 of the License, or (at your option) any later
+% version.
+%
+% This program is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+% details.
+%
+% You should have received a copy of the GNU General Public License along with
+% this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+% Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+% Last revision on: 09.10.2012 15:50
+
 MExc = ExceptionMessage( ...
     'NumArg', 'message', ...
     'The number of input parameters does not match the number of parsed args.');
