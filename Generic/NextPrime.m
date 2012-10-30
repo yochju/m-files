4e3af658c0d2a19627f0443d7bbc19b69e48b1f6
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-10-30 21:28:19 +0100

    Made the method slightly less stupid.

--- Generic/NextPrime.m
+++ Generic/NextPrime.m
@@ -1,6 +1,12 @@
 function out = NextPrime(in)
 out = in;
-while ~isprime(out)
+if out == 1
+    out = 2;
+elseif (mod(out,2) == 0)
     out = out + 1;
+end
+
+while ~isprime(out)||(out==in)
+    out = out + 2;
 end
 end
