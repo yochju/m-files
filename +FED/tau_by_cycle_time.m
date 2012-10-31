d6e3faee5717c609998a5e9607415e9c22704d73
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-10-31 09:04:48 +0100

    Updated Copyright and License information.

--- +FED/tau_by_cycle_time.m
+++ +FED/tau_by_cycle_time.m
@@ -1,4 +1,23 @@
 function out = tau_by_cycle_time(t, tau_max, reordering)
+
+% Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+%
+% This program is free software; you can redistribute it and/or modify it under
+% the terms of the GNU General Public License as published by the Free Software
+% Foundation; either version 3 of the License, or (at your option) any later
+% version.
+%
+% This program is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+% details.
+%
+% You should have received a copy of the GNU General Public License along with
+% this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+% Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+% Last revision on: 31.10.2012 9:00
+
 %n = fix(ceil(sqrt(3.0*t/tau_max+0.25)-0.5-1.0e-8)+0.5);
 
 % This is not exactly the same as in the code, but according to the theory it
