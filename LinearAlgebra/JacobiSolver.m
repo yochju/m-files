82e88b8b2a2905f7441e472260c55727d6ad4270
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-11-09 22:00:01 +0100

    Added simple implementations of iterative solvers.

---
+++ LinearAlgebra/JacobiSolver.m
@@ -0,0 +1,28 @@
+function out = JacobiSolver(A,b,it,x0)
+
+% Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+%
+% This program is free software; you can redistribute it and/or modify it under
+% the terms of the GNU General Public License as published by the Free Software
+% Foundation; either version 3 of the License, or (at your option) any later
+% version.
+%
+% This program is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+% details.
+%
+% You should have received a copy of the GNU General Public License along with
+% this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+% Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+% NOTE: Assumes A is sparse.
+
+D  = spdiags(A,0);
+UL = A - spdiags(D,0);
+out = x0;
+
+for i = 1:it
+    out = (b - UL*out)./D;
+end
+end
