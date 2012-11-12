feb4f65f7bd4d74fdee4a6013f3b9ff1c88406dd
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-11-12 21:14:48 +0100

    Completed implementation and documentation.

--- LinearAlgebra/JacobiSolver.m
+++ LinearAlgebra/JacobiSolver.m
@@ -1,4 +1,61 @@
-function out = JacobiSolver(A,b,it,x0)
+function [x, varargout] = JacobiSolver(A, b, varargin)
+%% Jacobi solver for linear systems.
+%
+% [x, varargout] = JacobiSolver(A, b, varargin)
+%
+% Input parameters (required):
+%
+% A : system matrix. (double or single array)
+% b : righthand side of the linear system. Can be a matrix too. In that case,
+%     the system is solved for every column. (double of single array) 
+%
+% Input parameters (optional):
+%
+% Optional parameters are either struct with the following fields and
+% corresponding values or option/value pairs, where the option is specified as a
+% string.
+%
+% x0    : initial vector to start iterations. Default is the zeros vector.
+%         (double or single array).
+% tol   : tolerance limit of relative residual. Default 1e-10. (scalar)
+% maxit : Maximal number of iterations. Default is 1. (integer)
+% relax : Relaxation parameter. Default is 1.0 (scalar)
+% M1    : Left preconditioning matrix. Default is identity matrix. (double or
+%         single array)
+% M2    : Right preconditioning matrix. Default is identity matrix. (double or
+%         single array)
+% check : whether to check if convergence is possible. Can increasy computation
+%         time significantly. Default is false. (boolean)
+%
+% Output parameters:
+%
+% x : Solution of the linear system.
+%
+% Output parameters (optional):
+%
+% flag   : flag indicating status of the process. Following values are possible.
+%          -1 : Convergence check found that method will not converge.
+%           0 : No problems occured.
+%           1 : Tolerance could not be reached with maxit iterations.
+% relres : relative residual of the last iterate. 0 if system is trivially
+%          solvable because righthand side was 0.
+% it     : number of performed iterations. If righthand side was 0, it = inf.
+% resvec : residual of the last iterate.
+%
+% Description:
+%
+% Tries to solve the linear system Ax=b using a Gauß Seidel Method.
+%
+% Example:
+%
+% Solve linear system Ax=b with 3 different righthand sides.
+%
+% T = randi(10,[3,3]);
+% A = T'*T + 200*eye(3);
+% b = randi(10,[3,1]);
+% [x1 x2 x3 x4 x5] = JacobiSolver(A,[b 2*b.^2],'maxit',10000)
+%
+% See also GaussSeidelSolver
 
 % Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
 %
@@ -16,13 +73,163 @@ function out = JacobiSolver(A,b,it,x0)
 % this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 % Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-% NOTE: Assumes A is sparse.
+% Last revision on: 12.11.2012 21:00
+
+%% Parse input.
+
+narginchk(2, 16);
+nargoutchk(1, 5);
+
+parser = inputParser;
+parser.FunctionName = mfilename;
+parser.CaseSensitive = false;
+parser.KeepUnmatched = true;
+parser.StructExpand = true;
+
+% System Matrix
+parser.addRequired('A', ...
+    @(x) validateattributes(x, {'single', 'double'}, ...
+    {'2d', 'nonempty', 'nonnan', 'finite'}, ...
+    mfilename, 'A'));
+
+% Righthand side. Can be a matrix too.
+parser.addRequired('b', ...
+    @(x) validateattributes(x, {'single', 'double'}, ...
+    {'2d', 'nonempty', 'nonnan', 'finite'}, ...
+    mfilename, 'b'));
+
+% Initial guess. If non is specified we initialise with 0.
+parser.addParamValue('x0', [], ...
+    @(x) validateattributes(x, {'single', 'double'}, ...
+    {'2d', 'nonempty', 'nonnan', 'finite', 'size', size(b)}, ...
+    mfilename, 'x0'));
+
+% Tolerance threshold for the relative residual.
+parser.addParamValue('tol', 1e-10, ...
+    @(x) validateattributes(x, {'numeric'}, ...
+    {'scalar', 'real', 'finite', 'nonnegative'}, ...
+    mfilename, 'tol'));
+
+% Maximal number of iterations.
+parser.addParamValue('maxit', 1, ...
+    @(x) validateattributes(x, {'numeric'}, ...
+    {'scalar', 'integer', 'finite', 'nonnegative'}, ...
+    mfilename, 'maxit'));
+
+% Relaxation parameter.
+parser.addParamValue('relax', 1.0, ...
+    @(x) validateattributes(x, {'numeric'}, ...
+    {'scalar', 'real', 'nonnegative', '<=', 2.0}, ...
+    mfilename, 'relax'));
+
+% Preconditioning Matrix. (Left preconditioner)
+parser.addParamValue('M1', [], ...
+    @(x) validateattributes(x, {'single', 'double'}, ...
+    {'2d', 'nonempty', 'nonnan', 'finite', 'size', size(A)}, ...
+    mfilename, 'M1'));
+
+% Preconditioning Matrix. (Right preconditioner)
+parser.addParamValue('M2', [], ...
+    @(x) validateattributes(x, {'single', 'double'}, ...
+    {'2d', 'nonempty', 'nonnan', 'finite', 'size', size(A)}, ...
+    mfilename, 'M2'));
+
+% Check whether method will converge. As this can be very slow, it is turned off
+% by default.
+parser.addParamValue('check', false, ...
+    @(x) validateattributes(x, {'logical'}, ...
+    {'scalar'}, mfilename, 'check'));
+
+parser.parse(A, b, varargin{:})
+opts = parser.Results;
+
+%% Initialise data.
+
+if ~isempty(opts.x0)
+    x = opts.x0;
+else
+    x = zeros(size(opts.b));
+end
+
+if ~isempty(opts.M1)
+    A = M1*A;
+    b = M2*b;
+end
+if ~isempty(opts.M2)
+    A = A*M2;
+end
+
+flag   = 0;
+it     = 1;
+resvec = A*x-b;
+% Since the righthand side can have multiple columns we need to get the norm
+% columnwise too. Hence it's not possible to use norm here. Same applies for the
+% computations of the residual and stopping criteria below.
+relres = sqrt(sum(resvec.^2))./sqrt(sum(x.^2));
 
 D  = spdiags(A,0);
 UL = spdiags(zeros(length(D)),0,A);
-out = x0;
 
-for i = 1:it
-    out = (b - UL*out)./D;
+%% Perform convergence check if desired.
+if opts.check
+    T = -(A-UL)\UL;
+    r = eigs(T,1);
+    if abs(r) >= 1
+        ExcM = ExceptionMessage('Input','Method may not converge.');
+        warning(ExcM.id,ExcM.message);
+    end
+    flag = -1;
 end
+
+%% Check for trivial solutions.
+if max(sqrt(sum(b.^2))) < 100*eps(max(sqrt(sum(b.^2))))
+    x = zeros(size(A,2),size(b,2));
+    resvec = x;
+    relres = 0;
+    it = inf;
+end
+
+%% Perform iterations.
+while ((it <= opts.maxit) && (max(relres) > opts.tol))
+    x = (1-opts.relax)*x + opts.relax*(b - UL*x)./kron(ones(1,size(b,2)),D);
+    if nargout >= 3
+        if ~isempty(opts.M2)
+            resvec = A*(opts.M2*x)-b;
+            relres = sqrt(sum(resvec.^2))./sqrt(sum((opts.M2*x).^2));
+        else
+            resvec = A*x-b;
+            relres = sqrt(sum(resvec.^2))./sqrt(sum(x.^2));
+        end
+    end
+    it = it + 1;
+end
+
+if ~isempty(opts.M2)
+    x = opts.M2*x;
+end
+
+%% If no other errors appeared and we reached maxit, set flag accordingly.
+if (it == opts.maxit) && (flag == 0)
+    flag = 1;
+end
+
+%% Set output vars. Follows output order of other standard matlab solvers.
+
+switch nargout
+    case 2
+        varargout{1} = flag;
+    case 3
+        varargout{1} = flag;
+        varargout{2} = relres;
+    case 4
+        varargout{1} = flag;
+        varargout{2} = relres;
+        varargout{3} = it;
+    case 5
+        varargout{1} = flag;
+        varargout{2} = relres;
+        varargout{3} = it;
+        varargout{4} = resvec;
+end
+
 end
