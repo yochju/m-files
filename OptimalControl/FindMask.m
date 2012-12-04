689a30535942de9be36bc4162f8031c265482bbe
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-12-04 15:05:24 +0100

    Some Improvements.

--- OptimalControl/FindMask.m
+++ OptimalControl/FindMask.m
@@ -2,7 +2,7 @@ function [u, c, its, flag, ...
     cEvo, uEvo, tEvo, ener] = FindMask(f, lambda, varargin)
 %% Find Optimal Mask for Homogeneous inpainting.
 %
-% [u, c, its, flag, cEvo, uEvo, tEvo, ener] = FindMask(f, lambda, varargin) 
+% [u, c, its, flag, cEvo, uEvo, tEvo, ener] = FindMask(f, lambda, varargin)
 %
 % Input Parameters (required):
 %
@@ -16,13 +16,15 @@ function [u, c, its, flag, ...
 % string.
 %
 % mu           : penalisation for the proximal term. (scalar, default = 1.0)
+% e            : quadratic penalisation term for the mask. (scalar, default =
+%                1-e3)
 % maxit        : maximal number of iterations. (integer, default = 1)
 % tol          : tolerance threshold (scalar, default 1e-3)
-% SplitBregman : options for the SplitBregman12 algorithm (struct, 
+% SplitBregman : options for the SplitBregman12 algorithm (struct,
 %                default = struct())
 % SolvePde     : options for the SolvePde algorithm (struct, default = struct())
-% Verbose      : whether information should be printed to stderr. (boolean,
-%                default = false)
+% Verbose      : whether information should be printed to stderr. Also displays
+%                a plot window with information. (boolean, default = false)
 % Save         : if non-empty, data is save to this file at each iteration.
 %                (string, default = '')
 %
@@ -71,11 +73,11 @@ function [u, c, its, flag, ...
 % this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 % Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-% Last revision on: 30.11.2012 10:30
+% Last revision on: 04.12.2012 11:15
 
 %% Check Input and Output
 
-narginchk(2,14);
+narginchk(2,16);
 nargoutchk(0,8);
 
 parser = inputParser;
@@ -94,6 +96,10 @@ parser.addRequired('lambda', @(x) validateattributes(x, ...
 parser.addParamValue('mu', 1.0, @(x) validateattributes(x, ...
     {'numeric'}, {'scalar','nonempty','finite','nonnegative'}, ...
     mfilename, 'mu'));
+
+parser.addParamValue('e', 1e-3, @(x) validateattributes(x, ...
+    {'numeric'}, {'scalar','nonempty','finite','nonnegative'}, ...
+    mfilename, 'e'));
 
 parser.addParamValue('maxit', 1, @(x) validateattributes(x, ...
     {'numeric'}, {'scalar','nonempty','finite','nonnegative', 'integer'}, ...
@@ -155,6 +161,7 @@ timeTotal = 0;
 
 data = struct( ...
     'iteration', cell(opts.maxit,1), ...
+    'lastiter', cell(opts.maxit,1), ...
     'mask', cell(opts.maxit,1), ...
     'solution', cell(opts.maxit,1), ...
     'cEvo', cell(opts.maxit,1), ...
@@ -234,31 +241,45 @@ while i <= opts.maxit
     catch err
         if opts.Verbose
             ExcM = ExceptionMessage('Internal', ...
-                'Construction of S or h might have failed.');
+                'message', 'Construction of S or h might have failed.');
             warning(ExcM.id,ExcM.message);
         end
     end
     warning(s);
     
     % Set up the matrix and righthand side in the P1 problem.
-    M = [ S ; opts.mu^2*speye(N,N) ];
-    l = [ h(:) ; opts.mu^2*cbar(:) ];
+    if opts.e > 10*eps
+        M = [ S ; opts.mu^2*speye(N,N) ; opts.e.*speye(N,N) ];
+        l = [ h(:) ; opts.mu^2*cbar(:) ; zeros(size(cbar(:))) ];
+    else
+        M = [ S ; opts.mu^2*speye(N,N) ];
+        l = [ h(:) ; opts.mu^2*cbar(:) ];
+    end
     
     % - Solve optimisation problem to get new mask ----------------------- %
     % argmin_c || M c - l ||_2^2 + lambda*|| c ||_1
     
-    [ c, fSpB, ~, ~, iSpB ] = Optimization.SplitBregman12( speye(N,N), zeros(N,1), ...
-        1/lambda, M, -l, opts.SplitBregman);
+    [ c, fSpB, ~, ~, iSpB ] = Optimization.SplitBregman12( speye(N,N), ...
+        zeros(N,1), 1/lambda, M, -l, opts.SplitBregman);
     if opts.Verbose
         fprintf(2,'\nSplit Bregman terminated after %d iterations.', iSpB);
         fprintf(2,'\nStopping reason was: %d', fSpB);
     end
-
+    
+    if (any(c<0)||any(c>1))
+        ExcM = ExceptionMessage('Internal', ...
+            'message', 'Mask went out of range!\n');
+        fprintf(2,'\n\n');
+        warning(ExcM.id,ExcM.message);
+        c(c<0) = 0;
+        c(c>1) = 1;
+    end
+    
     % - Compute solution corresponding to new mask. ---- %
-            
+    
     if norm(c) < 100*eps
         %% If c is 0, no need to compute a solution u. It must be 0.
-                
+        
         u = zeros(size(c));
         
     else
@@ -268,11 +289,31 @@ while i <= opts.maxit
         
     end
     
+    if opts.Verbose
+        figure(666);
+        x = linspace(0,1,numel(f));
+        subplot(2,2,1);
+        plot(x,f,x,u,x,c,'*r');
+        title('Current Solution.');
+        subplot(2,2,2);
+        plot(x,f,x,ubar,x,cbar,'*r');
+        title('Old Solution.');
+        subplot(2,2,[3, 4])
+        plot(ener(~isinf(ener)),'*r');
+        title('Energy');
+    end
+    
     if nargout > 4
         cEvo(i,:) = c';
         uEvo(i,:) = u';
         tEvo(i) = norm(c-cbar);
         ener(i) = Energy(u, c, f, lambda);
+        if (i>1)&&(ener(i) > ener(i-1))
+            ExcM = ExceptionMessage('Internal', ...
+                'message', 'Energy has increased!\n');
+            fprintf(2,'\n\n');
+            warning(ExcM.id,ExcM.message);
+        end
     end
     
     timeIter = toc();
@@ -287,12 +328,21 @@ while i <= opts.maxit
     
     if failsafe
         data(i).iteration = i;
+        
+        % This is evil...
+        temp = num2cell(i*ones(1,opts.maxit),[1 opts.maxit]);
+        [data(:).lastiter] = temp{:};
+        
         data(i).mask = c;
         data(i).solution = u;
         data(i).cEvo = cEvo;
         data(i).uEvo = uEvo;
         data(i).tEvo = tEvo;
         data(i).ener = ener;
+        data(i).cEvo((i+1):end,:) = [];
+        data(i).uEvo((i+1):end,:) = [];
+        data(i).tEvo(isnan(tEvo)) = [];
+        data(i).ener(isinf(ener)) = [];
         data(i).timeIter = timeIter;
         data(i).timeTotal = timeTotal;
         save(opts.Save, 'data');
