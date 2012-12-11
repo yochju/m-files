1f58462194380eff38bee0303601af9f3bd80dd9
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2012-12-11 15:56:17 +0100

    Adapted to changes in FiniteDiff1DM and DiffFilter1D.

--- LinearAlgebra/LaplaceM.m
+++ LinearAlgebra/LaplaceM.m
@@ -1,46 +1,56 @@
-function M = LaplaceM(r,c,varargin)
+function [M varargout] = LaplaceM(r,c,varargin)
 %% Returns the matrix corresponding to the Laplace operator.
 %
-% M = KroneckerSum(A,B)
-%
-% Computes the matrix corresponding to the Laplace operator. The discretisation
-% is based on separable finite difference schemes (which can be specified
-% through the options). The returned matrix is sparse. Boundary conditions can
-% be specified through optional parameters. Accepted values are 'Neumann' or
-% 'Dirichlet'.
+% M = LaplaceM(r, c, ...)
+% [M cons] = LaplaceM(r, c, ...)
 %
 % Input Parameters (required):
 %
 % r : number of rows of the signal. (positive integer)
 % c : number of columns of the signal. (positive integer)
 %
-% Input Parameters (pairs), (optional):
+% Input parameters (parameters):
 %
-% 'KnotsR'    : knots to consider for the row discretisation of the second
-%               derivative, e.g. x-derivative. (array of integers) (default
-%               [-1,0,1])
-% 'KnotsC'    : knots to consider for the column discretisation of the second
-%               derivative, e.g. y-derivative. (array of integers) (default
-%               [-1,0,1])
-% 'GridSizeR' : size of the grid for rows. (positive double) (default = 1.0)
-% 'GridSizeR' : size of the grid for columns. (positive double) (default = 1.0)
-% 'Boundary'  : boundary condition. (string) (default = 'Neumann')
-% 'Tolerance' : tolerance when checking the consistency order. (default 100*eps)
+% Parameters are either struct with the following fields and corresponding
+% values or option/value pairs, where the option is specified as a string.
 %
-% Output Parameters
+% knotsR : Knots to be considered for the rows. (array of sorted integers,
+%          default = [-1 0 1])
+% knotsC : Knots to be considered for the columns. (array of sorted integers,
+%          default = [-1 0 1])
+% optsR  : options to be passed to FiniteDiff1DM for the row computation.
+%          (struct, default = struct())
+% optsC  : options to be passed to FiniteDiff1DM for the column computation.
+%          (struct, default = struct())
+%
+% Input parameters (optional):
+%
+% The number of optional parameters is always at most one. If a function takes
+% an optional parameter, it does not take any other parameters.
+%
+% -
+%
+% Output Parameters:
 %
 % M : Matrix of the corresponding scheme. (sparse matrix)
 %
+% Output parameters (optional):
+%
+% cons : consistency order of the obtained scheme. (integer)
+%
+% Description:
+%
+% Computes the matrix corresponding to the Laplace operator. The discretisation
+% is based on separable finite difference schemes (which can be specified
+% through the options). The returned matrix is sparse. Boundary conditions can
+% be specified through optional parameters. Accepted values are 'Neumann' or
+% 'Dirichlet'. Default is the same as for FiniteDiff1DM.
+%
 % Example
 %
-% Remarks
+% LaplaceM(4,5);
 %
-% The implementation assumes that the points are number row-wise. This is in
-% conflict with the standard numbering scheme in MATLAB, which runs column-wise
-% over a matrix.
-% TODO: implement an option to make this behavior changeable.
-%
-% See also DiffFilter1DM.
+% See also DiffFilter1D, FiniteDiff1DM
 
 % Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
 %
@@ -58,57 +68,72 @@ function M = LaplaceM(r,c,varargin)
 % this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 % Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-% Last revision: 02.10.2012 12:20
+% Last revision: 11.12.2012 15:30
 
-%% Check input parameters
+%% Notes
+%
+% The implementation assumes that the points are number row-wise. This is in
+% conflict with the standard numbering scheme in MATLAB, which runs column-wise
+% over a matrix.
+% TODO: implement an option to make this behavior changeable.
 
-narginchk(2, 14);
+%% Parse input and output.
+
+narginchk(2, 10);
 nargoutchk(0, 1);
 
-optargin = size(varargin,2);
-stdargin = nargin - optargin;
+parser = inputParser;
+parser.FunctionName = mfilename;
+parser.CaseSensitive = false;
+parser.KeepUnmatched = true;
+parser.StructExpand = true;
 
-assert( stdargin == 2, ...
-    'LinearAlgebra:LaplaceM:BadInput', ...
-    ['The first two parameters must be the number of rows and columns of ' ...
-     'the signal.']);
-assert( mod(optargin,2)==0, ...
-    'LinearAlgebra:LaplaceM:BadInput', ...
-    'Optional arguments must come in pairs.');
+parser.addRequired('r', @(x) validateattributes(x, {'numeric'}, ...
+    {'scalar', 'integer', 'positive'}, mfilename, 'r'));
 
-KnotsR = [-1 0 1];
-GridSR = 1.0;
-KnotsC = [-1 0 1];
-GridSC = 1.0;
-BoundCond = 'Neumann';
-Tol = 100*eps;
-for i = 1:2:optargin
-    switch varargin{i}
-        case 'KnotsR'
-            KnotsR = varargin{i+1};
-        case 'KnotsC'
-            KnotsC = varargin{i+1};
-        case 'GridSizeR'
-            GridSR = varargin{i+1};
-        case 'GridSizeC'
-            GridSC = varargin{i+1};
-        case 'Boundary'
-            BoundCond = varargin{i+1};
-        case 'Tolerance'
-            Tol = varargin{i+1};
+parser.addRequired('c', @(x) validateattributes(x, {'numeric'}, ...
+    {'scalar', 'integer', 'positive'}, mfilename, 'c'));
+
+parser.addParamValue('knotsR', @(x) validateattributes(x, {'numeric'}, ...
+    {'vector', 'integer'}, mfilename, 'knotsR'));
+
+parser.addParamValue('knotsC', @(x) validateattributes(x, {'numeric'}, ...
+    {'vector', 'integer'}, mfilename, 'knotsC'));
+
+parser.addParamValue('optsR', struct(), @(x) validateattributes(x, ...
+    {'struct'}, {}, mfilename, 'optsR'));
+
+parser.addParamValue('optsC', struct(), @(x) validateattributes(x, ...
+    {'struct'}, {}, mfilename, 'optsC'));
+
+parser.parse(r, c, varargin{:});
+opts = parser.Results;
+
+%% Run code.
+
+if nargout < 2
+    MR = FiniteDiff1DM(c, opts.knotsR, 2, 'optsFilter', opts.optsR);
+    MC = FiniteDiff1DM(r, opts.knotsC, 2, 'optsFilter', opts.optsC);
+    if r == 1
+        M = MR;
+    elseif c == 1
+        M = MC;
+    else
+        M = KroneckerSum(MC,MR);
+    end
+else
+    [MR consR] = FiniteDiff1DM(c, opts.knotsR, 2, 'optsFilter', opts.optsR);
+    [MC consC] = FiniteDiff1DM(r, opts.knotsC, 2, 'optsFilter', opts.optsC);
+    if r == 1
+        M = MR;
+        varargout{1} = consR;
+    elseif c == 1
+        M = MC;
+        varargout{1} = consC;
+    else
+        M = KroneckerSum(MC,MR);
+        varargout{1} = min([consR, consC]);
     end
 end
 
-%% Set up internal variables.
-
-MR = FiniteDiff1DM(c, KnotsR, 2, ...
-    'GridSize', GridSR, 'Boundary', BoundCond, 'Tolerance', Tol);
-MC = FiniteDiff1DM(r, KnotsC, 2, ...
-    'GridSize', GridSC, 'Boundary', BoundCond, 'Tolerance', Tol);
-if r == 1
-    M = MR;
-elseif c == 1
-    M = MC;
-else
-    M = KroneckerSum(MC,MR);
 end
