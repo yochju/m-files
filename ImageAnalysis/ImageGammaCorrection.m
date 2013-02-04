857f865bc1c2a4f18210245522b4a57e8d5de222
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2013-02-04 21:09:38 +0100

    Update of code.

--- ImageAnalysis/ImageGammaCorrection.m
+++ ImageAnalysis/ImageGammaCorrection.m
@@ -1,31 +1,46 @@
 function out = ImageGammaCorrection( in , gamma )
-%% Short description.
+%% Performs gamma correction.
 %
-% out = NewFunction( in )
+% out = ImageGammaCorrection( in , gamma )
 %
 % Input parameters (required):
 %
+% in    : Input signal.
+% gamma : Gamma correction factor.
 %
+% Input parameters (parameters):
+%
+% Parameters are either struct with the following fields and corresponding
+% values or option/value pairs, where the option is specified as a string.
+%
+% -
 %
 % Input parameters (optional):
 %
+% The number of optional parameters is always at most one. If a function takes
+% an optional parameter, it does not take any other parameters.
 %
+% -
 %
 % Output parameters:
 %
+% out : Gamma corrected Image.
 %
+% Output parameters (optional):
+%
+% -
 %
 % Description:
 %
-%
+% Performs gamma correction according to the formula x.^(1/gamma).
 %
 % Example:
 %
-%
+% -
 %
 % See also
 
-% Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+% Copyright 2012, 2013 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
 %
 % This program is free software; you can redistribute it and/or modify it under
 % the terms of the GNU General Public License as published by the Free Software
@@ -41,13 +56,33 @@ function out = ImageGammaCorrection( in , gamma )
 % this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 % Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-% Last revision on: 04.06.2012 16:21
+% Last revision on: 04.02.2013 21:04
 
-%% Check Input and Output Arguments
+%% Notes
 
-%% Algorithm
+%% Parse input and output.
 
-fun = @(x) x.^(1/gamma);
-out = ImagePointTransform(in,fun);
+narginchk(2,2);
+nargoutchk(0,1);
+
+parser = inputParser;
+parser.FunctionName = mfilename;
+parser.CaseSensitive = false;
+parser.KeepUnmatched = true;
+parser.StructExpand = true;
+
+parser.addRequired('in', @(x) validateattributes( x, {'numeric'}, ...
+    {'2d', 'finite', 'nonempty', 'nonnan', 'nonsparse'}, ...
+    mfilename, 'in', 1) );
+
+parser.addRequired('gamma', @(x) validateattributes( x, {'numeric'}, ...
+    {'scalar', 'finite', 'nonempty', 'nonnan', 'nonzero'}, ...
+    mfilename, 'gamma', 2) );
+
+parser.Results;
+
+%% Run code.
+
+out = in.^(1/gamma);
 
 end
