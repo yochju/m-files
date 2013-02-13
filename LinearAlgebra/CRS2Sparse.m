44f0cd131880c6d6c2c0033864d9386e44755075
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2013-02-13 16:29:43 +0100

    Added functions to convert compressed column and compressed row matrix format.

---
+++ LinearAlgebra/CRS2Sparse.m
@@ -0,0 +1,67 @@
+function out = CRS2Sparse(ir, jc, v, nr, nc)
+%% <H1 LINE>
+%
+% <SIGNATURE>
+%
+% Input parameters (required):
+%
+% -
+%
+% Input parameters (parameters):
+%
+% Parameters are either struct with the following fields and corresponding
+% values or option/value pairs, where the option is specified as a string.
+%
+% -
+%
+% Input parameters (optional):
+%
+% The number of optional parameters is always at most one. If a function takes
+% an optional parameter, it does not take any other parameters.
+%
+% -
+%
+% Output parameters:
+%
+% -
+%
+% Output parameters (optional):
+%
+% -
+%
+% Description:
+%
+% -
+%
+% Example:
+%
+% -
+%
+% See also
+
+% Copyright 2013 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+%
+% This program is free software; you can redistribute it and/or modify it under
+% the terms of the GNU General Public License as published by the Free Software
+% Foundation; either version 3 of the License, or (at your option) any later
+% version.
+%
+% This program is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+% details.
+%
+% You should have received a copy of the GNU General Public License along with
+% this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+% Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+% Last revision on: 13.02.2013 15:41
+
+%% Notes
+
+%% Parse input and output.
+
+%% Run code.
+
+out = SetSparseData(v, jc, ir, nr, nc)';
+end
