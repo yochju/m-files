8686a6f3b0d802a430b91d1cfe12cdbecc061568
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2013-05-20 11:18:30 +0200

    Fixed doc and changed default labeling from rowwise to colwise.

--- LinearAlgebra/LaplaceM.m
+++ LinearAlgebra/LaplaceM.m
@@ -14,14 +14,20 @@ function [M varargout] = LaplaceM(r,c,varargin)
 % Parameters are either struct with the following fields and corresponding
 % values or option/value pairs, where the option is specified as a string.
 %
-% knotsR : Knots to be considered for the rows. (array of sorted integers,
-%          default = [-1 0 1])
-% knotsC : Knots to be considered for the columns. (array of sorted integers,
-%          default = [-1 0 1])
-% optsR  : options to be passed to FiniteDiff1DM for the row computation.
-%          (struct, default = struct())
-% optsC  : options to be passed to FiniteDiff1DM for the column computation.
-%          (struct, default = struct())
+% boundaryR : boundary condition for rows. (string, default = 'Neumann')
+% boundaryC : boundary condition for columns. (string, default = 'Neumann')
+% knotsR    : Knots to be considered for the rows. (array of sorted integers,
+%             default = [-1 0 1])
+% knotsC    : Knots to be considered for the columns. (array of sorted integers,
+%             default = [-1 0 1])
+% optsR     : options to be passed to DiffFilter1D for the row computation.
+%             (struct, default = struct())
+% optsC     : options to be passed to DiffFilter1D for the column computation.
+%             (struct, default = struct())
+% labeling  : how the pixels in the image are labelled. Either 'row' for rowwise
+%             numbering or 'col' for columnwise numbering. The default is to
+%             number the signal entries columnwise (e.g. matlab default)
+%             (string, default = 'col')
 %
 % Input parameters (optional):
 %
@@ -52,7 +58,7 @@ function [M varargout] = LaplaceM(r,c,varargin)
 %
 % See also DiffFilter1D, FiniteDiff1DM
 
-% Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+% Copyright 2012, 2013 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
 %
 % This program is free software; you can redistribute it and/or modify it under
 % the terms of the GNU General Public License as published by the Free Software
@@ -68,18 +74,13 @@ function [M varargout] = LaplaceM(r,c,varargin)
 % this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 % Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-% Last revision: 11.12.2012 15:30
+% Last revision: 20.05.2013 10:36
 
 %% Notes
-%
-% The implementation assumes that the points are number row-wise. This is in
-% conflict with the standard numbering scheme in MATLAB, which runs column-wise
-% over a matrix.
-% TODO: implement an option to make this behavior changeable.
 
 %% Parse input and output.
 
-narginchk(2, 10);
+narginchk(2, 16);
 nargoutchk(0, 2);
 
 parser = inputParser;
@@ -94,6 +95,14 @@ parser.addRequired('r', @(x) validateattributes(x, {'numeric'}, ...
 parser.addRequired('c', @(x) validateattributes(x, {'numeric'}, ...
     {'scalar', 'integer', 'positive'}, mfilename, 'c'));
 
+parser.addParamValue('boundaryR', 'Neumann', ...
+    @(x) strcmpi(x, validatestring(x, {'Dirichlet', 'Neumann'}, mfilename, ...
+    'boundaryR')));
+
+parser.addParamValue('boundaryC', 'Neumann', ...
+    @(x) strcmpi(x, validatestring(x, {'Dirichlet', 'Neumann'}, mfilename, ...
+    'boundaryC')));
+
 parser.addParamValue('knotsR', [-1 0 1], @(x) validateattributes(x, ...
     {'numeric'}, {'vector', 'integer'}, mfilename, 'knotsR'));
 
@@ -106,32 +115,58 @@ parser.addParamValue('optsR', struct(), @(x) validateattributes(x, ...
 parser.addParamValue('optsC', struct(), @(x) validateattributes(x, ...
     {'struct'}, {}, mfilename, 'optsC'));
 
+parser.addParamValue('labeling', 'row', ...
+    @(x) strcmpi(x, validatestring(x, {'col', 'row'}, mfilename, ...
+    'labeling')));
+
 parser.parse(r, c, varargin{:});
 opts = parser.Results;
 
 %% Run code.
 
 if nargout < 2
-    MR = FiniteDiff1DM(c, opts.knotsR, 2, 'optsFilter', opts.optsR);
-    MC = FiniteDiff1DM(r, opts.knotsC, 2, 'optsFilter', opts.optsC);
+    % Applies derivatives along rows (e.g. signals of length c)
+    MR = FiniteDiff1DM(c, opts.knotsR, 2, 'boundary', opts.boundaryR, ...
+        'optsFilter', opts.optsR);
+    % Applies derivatives along columns (e.g. signals of length r)
+    MC = FiniteDiff1DM(r, opts.knotsC, 2, 'boundary', opts.boundaryC, ...
+        'optsFilter', opts.optsC);
     if r == 1
+        % 2D/1D Signal consisting of a single row.
         M = MR;
     elseif c == 1
+        % 2D/1D Signal consisting of a single column.
         M = MC;
     else
-        M = KroneckerSum(MC,MR);
+        % M = kron(Dxx,I) + kron(I,Dyy)
+        if strcmpi(opts.labeling,'col')
+            M = KroneckerSum(MR,MC);
+        else
+            M = KroneckerSum(MC,MR);
+        end
     end
 else
-    [MR consR] = FiniteDiff1DM(c, opts.knotsR, 2, 'optsFilter', opts.optsR);
-    [MC consC] = FiniteDiff1DM(r, opts.knotsC, 2, 'optsFilter', opts.optsC);
+    [MR consR] = FiniteDiff1DM(c, opts.knotsR, 2, ...
+        'boundary', opts.boundaryR, ...
+        'optsFilter', opts.optsR);
+    [MC consC] = FiniteDiff1DM(r, opts.knotsC, 2, ...
+        'boundary', opts.boundaryC, ...
+        'optsFilter', opts.optsC);
     if r == 1
+        % 2D/1D Signal consisting of a single row.
         M = MR;
         varargout{1} = consR;
     elseif c == 1
+        % 2D/1D Signal consisting of a single column.
         M = MC;
         varargout{1} = consC;
     else
-        M = KroneckerSum(MC,MR);
+        % M = kron(Dxx,I) + kron(I,Dyy)
+        if strcmpi(opts.labeling,'col')
+            M = KroneckerSum(MR,MC);
+        else
+            M = KroneckerSum(MC,MR);
+        end
         varargout{1} = min([consR, consC]);
     end
 end
