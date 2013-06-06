eadebf9ac53eb123e5361c42f2c95aff9e18a455
Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
2013-06-06 12:13:10 +0200

    Added preliminary implementations of Householder and Givens matrices.

---
+++ LinearAlgebra/HouseholderM.m
@@ -0,0 +1,70 @@
+function M = HouseholderM(v)
+%% Return the Matrix corresponding to the Householder transform.
+%
+% <SIGNATURE>
+%
+% Input parameters (required):
+%
+% -
+%
+% Input parameters (parameters):
+%
+% Parameters are either struct with the following fields and corresponding
+% values or option/value pairs, where the option is specified as a string.
+%
+% -
+%
+% Input parameters (optional):
+%
+% The number of optional parameters is always at most one. If a function takes
+% an optional parameter, it does not take any other parameters.
+%
+% -
+%
+% Output parameters:
+%
+% -
+%
+% Output parameters (optional):
+%
+% -
+%
+% Description:
+%
+% -
+%
+% Example:
+%
+% -
+%
+% See also
+
+% Copyright 2013 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+%
+% This program is free software; you can redistribute it and/or modify it under
+% the terms of the GNU General Public License as published by the Free Software
+% Foundation; either version 3 of the License, or (at your option) any later
+% version.
+%
+% This program is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+% details.
+%
+% You should have received a copy of the GNU General Public License along with
+% this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+% Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+% Last revision on: 06.06.2013 12:10
+
+%% Notes
+
+%% Parse input and output.
+
+%% Run code.
+
+n = numel(v);
+v = v(:);
+M = speye(n,n) - 2*(v*v')/(v'*v);
+		       
+end
