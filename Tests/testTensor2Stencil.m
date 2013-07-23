9b8d01b59430d832bf5ddcbac8f7b3dab1f4b55e
Laurent Hoeltgen <hoeltgen@mia.uni-saarland.de>
2013-07-23 17:29:10 +0200

    Improved tests.

--- Tests/testTensor2Stencil.m
+++ Tests/testTensor2Stencil.m
@@ -1,7 +1,10 @@
-function testTensor2Stencil
+function test_suite = testTensor2Stencil
+
+initTestSuite;
+
+function test_ap0bp0cp0aap0bbp0
 nr = 3;
 nc = 3;
-%%
 a = 0*ones(nr, nc);
 b = 0*ones(nr, nc);
 c = 0*ones(nr, nc);
@@ -15,12 +18,15 @@ st = [ ...
     S{3,1}(1,1) S{3,2}(1,1) S{3,3}(1,1) ];
 assertElementsAlmostEqual(st, [0 0 0 ; 0 0 0 ; 0 0 0], ...
     'absolute', 1e-10, 1e-10);
-%%
+
+function test_ap1bp0cp0aap0bbp0
+nr = 3;
+nc = 3;
 a = 1*ones(nr, nc);
 b = 0*ones(nr, nc);
 c = 0*ones(nr, nc);
 alpha = 0*ones(nr, nc);
-beta  = 0x*ones(nr, nc);
+beta  = 0*ones(nr, nc);
 h = 1/sqrt(2);
 S =Tensor2Stencil(a,b,c,alpha,beta,'size',h);
 st = [ ...
@@ -29,7 +35,10 @@ st = [ ...
     S{3,1}(1,1) S{3,2}(1,1) S{3,3}(1,1) ];
 assertElementsAlmostEqual(st, [0 0 0 ; 2 -4 2 ; 0 0 0], ...
     'absolute', 1e-10, 1e-10);
-%%
+
+function test_ap0bp1cp0aap0bbp0
+nr = 3;
+nc = 3;
 a = 0*ones(nr, nc);
 b = 1*ones(nr, nc);
 c = 0*ones(nr, nc);
@@ -43,7 +52,10 @@ st = [ ...
     S{3,1}(1,1) S{3,2}(1,1) S{3,3}(1,1) ];
 assertElementsAlmostEqual(st, [1 0 -1 ; 0 0 0 ; -1 0 1], ...
     'absolute', 1e-10, 1e-10);
-%%
+
+function test_ap0bp0cp1aap0bbp0
+nr = 3;
+nc = 3;
 a = 0*ones(nr, nc);
 b = 0*ones(nr, nc);
 c = 1*ones(nr, nc);
@@ -57,7 +69,10 @@ st = [ ...
     S{3,1}(1,1) S{3,2}(1,1) S{3,3}(1,1) ];
 assertElementsAlmostEqual(st, [0 2 0 ; 0 -4 0 ; 0 2 0], ...
     'absolute', 1e-10, 1e-10);
-%%
+
+function test_ap1bp1cp0aap0bbp0
+nr = 3;
+nc = 3;
 a = 1*ones(nr, nc);
 b = 1*ones(nr, nc);
 c = 0*ones(nr, nc);
@@ -71,7 +86,10 @@ st = [ ...
     S{3,1}(1,1) S{3,2}(1,1) S{3,3}(1,1) ];
 assertElementsAlmostEqual(st, [1 0 -1 ; 2 -4 2 ; -1 0 1], ...
     'absolute', 1e-10, 1e-10);
-%%
+
+function test_ap1bp0cp1aap0bbp0
+nr = 3;
+nc = 3;
 a = 1*ones(nr, nc);
 b = 0*ones(nr, nc);
 c = 1*ones(nr, nc);
@@ -85,7 +103,10 @@ st = [ ...
     S{3,1}(1,1) S{3,2}(1,1) S{3,3}(1,1) ];
 assertElementsAlmostEqual(st, [0 2 0 ; 2 -8 2 ; 0 2 0], ...
     'absolute', 1e-10, 1e-10);
-%%
+
+function test_ap1bp1cp1aap0bbp0
+nr = 3;
+nc = 3;
 a = 1*ones(nr, nc);
 b = 1*ones(nr, nc);
 c = 1*ones(nr, nc);
@@ -99,7 +120,10 @@ st = [ ...
     S{3,1}(1,1) S{3,2}(1,1) S{3,3}(1,1) ];
 assertElementsAlmostEqual(st, [1 2 -1 ; 2 -8 2 ; -1 2 1], ...
     'absolute', 1e-10, 1e-10);
-%%
+
+function test_ap1bm1cp1aap0bpb0
+nr = 3;
+nc = 3;
 a = 1*ones(nr, nc);
 b = -1*ones(nr, nc);
 c = 1*ones(nr, nc);
@@ -113,4 +137,37 @@ st = [ ...
     S{3,1}(1,1) S{3,2}(1,1) S{3,3}(1,1) ];
 assertElementsAlmostEqual(st, [-1 2 1 ; 2 -8 2 ; 1 2 -1], ...
     'absolute', 1e-10, 1e-10);
-end
+
+function test_nr1nc1
+nr = 1;
+nc = 1;
+a = 1*ones(nr, nc);
+b = 0*ones(nr, nc);
+c = 1*ones(nr, nc);
+alpha = 0*ones(nr, nc);
+beta  = 0*ones(nr, nc);
+h = 1/sqrt(2);
+S =Tensor2Stencil(a,b,c,alpha,beta,'size',h);
+st = [ ...
+    S{1,1}(1,1) S{1,2}(1,1) S{1,3}(1,1) ; ...
+    S{2,1}(1,1) S{2,2}(1,1) S{2,3}(1,1) ; ...
+    S{3,1}(1,1) S{3,2}(1,1) S{3,3}(1,1) ];
+assertElementsAlmostEqual(st, [0 2 0 ; 2 -8 2 ; 0 2 0], ...
+    'absolute', 1e-10, 1e-10);
+
+function test_nrRANDncRAND
+nr = randi([2,1000],1);
+nc = randi([2,1000],1);
+a = 1*ones(nr, nc);
+b = 0*ones(nr, nc);
+c = 1*ones(nr, nc);
+alpha = 0*ones(nr, nc);
+beta  = 0*ones(nr, nc);
+h = 1/sqrt(2);
+S =Tensor2Stencil(a,b,c,alpha,beta,'size',h);
+st = [ ...
+    S{1,1}(1,1) S{1,2}(1,1) S{1,3}(1,1) ; ...
+    S{2,1}(1,1) S{2,2}(1,1) S{2,3}(1,1) ; ...
+    S{3,1}(1,1) S{3,2}(1,1) S{3,3}(1,1) ];
+assertElementsAlmostEqual(st, [0 2 0 ; 2 -8 2 ; 0 2 0], ...
+    'absolute', 1e-10, 1e-10);
--- Tests/testnorm0.m
+++ Tests/testnorm0.m
@@ -1,18 +1,27 @@
-function testnorm0
+function test_suite = testnorm0
+
+initTestSuite;
+
+function test_normal
 assertEqual(norm0([2 -3 4] ), 3);
 assertEqual(norm0([2 -3 4]'), 3);
-
 assertEqual(norm0([-6 2 0] ), 2);
 assertEqual(norm0([-6 2 0]'), 2);
-
-assertEqual(norm0([1e-10 0 0] ), 1);
-assertEqual(norm0([1e-10 0 0]'), 1);
-
 assertEqual(norm0([0 0 0] ), 0);
 assertEqual(norm0([0 0 0]'), 0);
 
+function test_small
+assertEqual(norm0([1e-10 0 0] ), 1);
+assertEqual(norm0([1e-10 0 0]'), 1);
 assertEqual(norm0([eps -eps ; 0 0]), 2);
 assertEqual(norm0([0 eps ; eps 0]), 2);
 
-assertEqual(norm0([1e100 -1e00 ; 0 -10 ; eps -eps]), 5);
-end
+function test_large
+assertEqual(norm0([1e20 0 0] ), 1);
+assertEqual(norm0([1e100 0 0]'), 1);
+assertEqual(norm0([1e20 -1e20 ; 0 0]), 2);
+assertEqual(norm0([0 1e100 ; -1e100 0]), 2);
+
+function test_mixed
+assertEqual(norm0([1e100 -1e100 ; 0 -10 ; eps -eps]), 5);
+
