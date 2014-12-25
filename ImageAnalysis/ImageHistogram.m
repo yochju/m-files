39008201ed7b1b175992e77a8a2dcae984088404
Laurent Hoeltgen <hoeltgman@gmail.com>
2014-12-25 10:44:40 +0100

    Added skeleton function for image histogram.

---
+++ ImageAnalysis/ImageHistogram.m
@@ -0,0 +1,71 @@
+function [out, varargout] = ImageHistogram(in, nBins, minR, maxR)
+%% <H1 LINE>
+%
+% <SIGNATURE>
+%
+% Input parameters (required):
+%
+% -
+%
+% Input parameters (parameters):
+%
+% Parameters are either struct with the following fields and corresponding
+% values or option/value pairs, where the option is specified as a string.
+%
+% -
+%
+% Input parameters (optional):
+%
+% The number of optional parameters is always at most one. If a function takes
+% an optional parameter, it does not take any other parameters.
+%
+% -
+%
+% Output parameters:
+%
+% -
+%
+% Output parameters (optional):
+%
+% -
+%
+% Description:
+%
+% -
+%
+% Example:
+%
+% -
+%
+% See also
+
+% Copyright 2013 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+%
+% This program is free software; you can redistribute it and/or modify it under
+% the terms of the GNU General Public License as published by the Free Software
+% Foundation; either version 3 of the License, or (at your option) any later
+% version.
+%
+% This program is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+% details.
+%
+% You should have received a copy of the GNU General Public License along with
+% this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+% Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+% Last revision on: dd.mm.yyyy hh:mm
+
+%% Notes
+
+%% Parse input and output.
+
+%% Run code.	
+
+I = in(:);
+J = linspace(minR,maxR,nBins);
+out = interp1(J,J,I,'nearest','extrap');
+% use histc...
+		       
+end
