eb6a09f5f1c992f87b95250feb4dba6be9a3c463
Laurent Hoeltgen <hoeltgman@gmail.com>
2014-12-28 19:49:07 +0100

    Update to new calling convention.

--- LinearAlgebra/LaplaceM.m
+++ LinearAlgebra/LaplaceM.m
@@ -1,4 +1,4 @@
-function [M varargout] = LaplaceM(r,c,varargin)
+function [M, varargout] = LaplaceM(r,c,varargin)
 %% Returns the matrix corresponding to the Laplace operator.
 %
 % M = LaplaceM(r, c, ...)
@@ -63,7 +63,7 @@ function [M varargout] = LaplaceM(r,c,varargin)
 %
 % See also DiffFilter1D, FiniteDiff1DM, GradientM
 
-% Copyright 2012, 2013 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+% Copyright 2012-2014 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
 %
 % This program is free software; you can redistribute it and/or modify it under
 % the terms of the GNU General Public License as published by the Free Software
@@ -79,7 +79,7 @@ function [M varargout] = LaplaceM(r,c,varargin)
 % this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 % Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-% Last revision: 20.05.2013 10:36
+% Last revision: 28.12.2014 19:47
 
 %% Notes
 
@@ -94,34 +94,34 @@ parser.CaseSensitive = false;
 parser.KeepUnmatched = true;
 parser.StructExpand = true;
 
-parser.addRequired('r', @(x) validateattributes(x, {'numeric'}, ...
+parser.addRequired('r', @(x) validateattributes (x, {'numeric'}, ...
     {'scalar', 'integer', 'positive'}, mfilename, 'r'));
 
-parser.addRequired('c', @(x) validateattributes(x, {'numeric'}, ...
+parser.addRequired('c', @(x) validateattributes (x, {'numeric'}, ...
     {'scalar', 'integer', 'positive'}, mfilename, 'c'));
 
 parser.addParamValue('boundaryR', 'Neumann', ...
-    @(x) strcmpi(x, validatestring(x, {'Dirichlet', 'Neumann'}, mfilename, ...
+    @(x) strcmpi (x, validatestring(x, {'Dirichlet', 'Neumann'}, mfilename, ...
     'boundaryR')));
 
 parser.addParamValue('boundaryC', 'Neumann', ...
-    @(x) strcmpi(x, validatestring(x, {'Dirichlet', 'Neumann'}, mfilename, ...
+    @(x) strcmpi (x, validatestring(x, {'Dirichlet', 'Neumann'}, mfilename, ...
     'boundaryC')));
 
-parser.addParamValue('knotsR', [-1 0 1], @(x) validateattributes(x, ...
+parser.addParamValue('knotsR', [-1 0 1], @(x) validateattributes (x, ...
     {'numeric'}, {'vector', 'integer'}, mfilename, 'knotsR'));
 
-parser.addParamValue('knotsC', [-1 0 1], @(x) validateattributes(x, ...
+parser.addParamValue('knotsC', [-1 0 1], @(x) validateattributes (x, ...
     {'numeric'}, {'vector', 'integer'}, mfilename, 'knotsC'));
 
-parser.addParamValue('optsR', struct(), @(x) validateattributes(x, ...
+parser.addParamValue('optsR', struct(), @(x) validateattributes (x, ...
     {'struct'}, {}, mfilename, 'optsR'));
 
-parser.addParamValue('optsC', struct(), @(x) validateattributes(x, ...
+parser.addParamValue('optsC', struct(), @(x) validateattributes (x, ...
     {'struct'}, {}, mfilename, 'optsC'));
 
 parser.addParamValue('labeling', 'col', ...
-    @(x) strcmpi(x, validatestring(x, {'col', 'row'}, mfilename, ...
+    @(x) strcmpi(x, validatestring (x, {'col', 'row'}, mfilename, ...
     'labeling')));
 
 parser.parse(r, c, varargin{:});
@@ -131,11 +131,11 @@ opts = parser.Results;
 
 if nargout < 2
     % Applies derivatives along rows (e.g. signals of length c)
-    MR = FiniteDiff1DM(c, opts.knotsR, 2, 'boundary', opts.boundaryR, ...
-        'optsFilter', opts.optsR);
+    MR = FiniteDiff1DM (c, opts.knotsR, 2, 'boundary', opts.boundaryR, ...
+        opts.optsR);
     % Applies derivatives along columns (e.g. signals of length r)
-    MC = FiniteDiff1DM(r, opts.knotsC, 2, 'boundary', opts.boundaryC, ...
-        'optsFilter', opts.optsC);
+    MC = FiniteDiff1DM (r, opts.knotsC, 2, 'boundary', opts.boundaryC, ...
+        opts.optsC);
     if r == 1
         % 2D/1D Signal consisting of a single row.
         M = MR;
@@ -144,19 +144,17 @@ if nargout < 2
         M = MC;
     else
         % M = kron(Dxx,I) + kron(I,Dyy)
-        if strcmpi(opts.labeling,'col')
-            M = KroneckerSum(MR,MC);
+        if strcmpi (opts.labeling, 'col')
+            M = KroneckerSum (MR, MC);
         else
-            M = KroneckerSum(MC,MR);
+            M = KroneckerSum (MC, MR);
         end
     end
 else
-    [MR consR] = FiniteDiff1DM(c, opts.knotsR, 2, ...
-        'boundary', opts.boundaryR, ...
-        'optsFilter', opts.optsR);
-    [MC consC] = FiniteDiff1DM(r, opts.knotsC, 2, ...
-        'boundary', opts.boundaryC, ...
-        'optsFilter', opts.optsC);
+    [MR, consR] = FiniteDiff1DM (c, opts.knotsR, 2, ...
+        'boundary', opts.boundaryR, opts.optsR);
+    [MC, consC] = FiniteDiff1DM (r, opts.knotsC, 2, ...
+        'boundary', opts.boundaryC, opts.optsC);
     if r == 1
         % 2D/1D Signal consisting of a single row.
         M = MR;
@@ -166,13 +164,13 @@ else
         M = MC;
         varargout{1} = consC;
     else
-        % M = kron(Dxx,I) + kron(I,Dyy)
-        if strcmpi(opts.labeling,'col')
-            M = KroneckerSum(MR,MC);
+        % M = kron (Dxx, I) + kron (I, Dyy)
+        if strcmpi (opts.labeling, 'col')
+            M = KroneckerSum (MR, MC);
         else
-            M = KroneckerSum(MC,MR);
+            M = KroneckerSum (MC, MR);
         end
-        varargout{1} = min([consR, consC]);
+        varargout{1} = min ([consR, consC]);
     end
 end
 
