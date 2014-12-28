03569f02b9d759bb8f57eb9c24a16f7b3920ebeb
Laurent Hoeltgen <hoeltgman@gmail.com>
2014-12-28 17:36:26 +0100

    Improved input parsing.

--- LinearAlgebra/KroneckerSum.m
+++ LinearAlgebra/KroneckerSum.m
@@ -1,7 +1,7 @@
-function M = KroneckerSum(A,B)
+function M = KroneckerSum (A, B)
 %% Computes Kronecker sum of two square matrices
 %
-% M = KroneckerSum(A,B)
+% M = KroneckerSum (A, B)
 %
 % Computes the Kronecker sum of two square matrices. The returned matrix is
 % sparse.
@@ -19,7 +19,7 @@ function M = KroneckerSum(A,B)
 %
 % A = [1 2 ; 3 4];
 % B = [5 6 ; 7 8];
-% M = KroneckerSum(A,B)
+% M = KroneckerSum (A, B)
 %
 % Remarks
 %
@@ -29,7 +29,7 @@ function M = KroneckerSum(A,B)
 %
 % See also kron.
 
-% Copyright 2012 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
+% Copyright 2012-2014 Laurent Hoeltgen <laurent.hoeltgen@gmail.com>
 %
 % This program is free software; you can redistribute it and/or modify it under
 % the terms of the GNU General Public License as published by the Free Software
@@ -45,24 +45,34 @@ function M = KroneckerSum(A,B)
 % this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 % Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-% Last revision: 2012/02/18 20:00
+% Last revision: 28.12.2014 17:00
 
 %% Comments and Remarks.
 %
 
 %% Check input parameters
 
-narginchk(2, 2);
-nargoutchk(0, 1);
+narginchk (2, 2);
+nargoutchk (0, 1);
 
-[ra ca] = size(A);
-[rb cb] = size(B);
-assert( IsSquarematrix(A) && IsSquarematrix(B), ...
-    'LinearAlgebra:KroneckerSum:BadInput', ...
-    'Input matrices must be square.');
+parser = inputParser;
+parser.FunctionName = mfilename;
+parser.CaseSensitive = false;
+parser.KeepUnmatched = false;
+parser.StructExpand = true;
+
+parser.addRequired('A', @(x) validateattributes (x, {'numeric'}, ...
+    {'2d', 'square', 'nonempty', 'nonnan', 'finite'}, mfilename, 'A'));
+parser.addRequired('B', @(x) validateattributes (x, {'numeric'}, ...
+    {'2d', 'square', 'nonempty', 'nonnan', 'finite'}, mfilename, 'A'));
+
+parser.parse(A, B);
+
+[ra, ca] = size (A);
+[rb, cb] = size (B);
 
 %% Compute the matrix
 
-M = kron(A,speye(rb,cb))+kron(speye(ra,ca),B);
+M = kron (A, speye (rb, cb)) + kron (speye (ra, ca), B);
 
 end
