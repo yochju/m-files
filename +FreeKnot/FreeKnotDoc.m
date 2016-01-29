e046eae02f83894b80c297bd9ef7fd3ef7870226
Laurent Hoeltgen <hoeltgman@gmail.com>
2016-01-29 09:25:36 +0100

    Added draft documentation.

---
+++ +FreeKnot/FreeKnotDoc.m
@@ -0,0 +1,44 @@
+%% Optimal Free Knots
+%
+%% Licensing
+% The complete source code from the files listed above is licensed under GPLv3
+% or later, wehreas this documentation is licensed as
+%
+%%
+% 
+%  Copyright (C) 2016 Laurent Hoeltgen <hoeltgen@b-tu.de>
+%  Permission is granted to copy, distribute and/or modify this document under
+%  the terms of the GNU Free Documentation License, Version 1.3 or any later
+%  version published by the Free Software Foundation; with no Invariant
+%  Sections, no Front-Cover Texts, and no Back-Cover Texts. A copy of the
+%  license is included in the section entitled "GNU Free Documentation License".
+%
+%
+%% Description
+% This matlab package contains 4 functions.
+% 
+% # OptInterp
+% # OptApprox
+% # ErrorInterp
+% # ErrorApprox
+%
+% They constitute an implementation of the algorithms presented in the following
+% publications:
+%
+% # H. Hamideh, On the optimal knots of first degree splines.
+% Kuwait Journal of Science and Engineering 29(1) (2002), pp. 1–13
+% # L. Hoeltgen, Continuous Spatial and Tonal Point Optimisation for
+% Interpolation and Approximation of Convex Signals with Homogeneous Diffusion
+% 2013
+%
+%% OptInterp
+% DESCRIPTIVE TEXT
+%
+%% OptApprox
+% DESCRIPTIVE TEXT
+%
+%% ErrorInterp
+% DESCRIPTIVE TEXT
+%
+%% ErrorApprox
+% DESCRIPTIVE TEXT
