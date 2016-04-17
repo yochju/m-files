54625f07bb7de5ba6b4501a68cf6fde1a696149e
Laurent Hoeltgen <hoeltgman@gmail.com>
2016-04-17 18:16:03 +0200

    Mex Function for cumsum
    Interface cumsum fortran code through mex.

---
+++ mex/mexcumsum.c
@@ -0,0 +1,35 @@
+#include "mex.h"
+
+extern long mexcumsum(long, double *, double *);
+
+void mexFunction( int nlhs,       mxArray *plhs[],
+                  int nrhs, const mxArray *prhs[] 
+		  )
+{
+  double *in, *out;        
+  mwSize nc, nr;
+    
+  /* Check I/O number */
+  if (nlhs > 1) {
+    mexErrMsgTxt("Incorrect number of outputs");
+  }
+  if (nrhs!=1) {
+    mexErrMsgTxt("Incorrect number of inputs");
+  }
+    
+  /* Brief error checking */
+
+  nr = mxGetM(prhs[0]);  
+  nc = mxGetN(prhs[0]);
+    
+  /* Allocate output */
+  plhs[0] = mxCreateDoubleMatrix(nr, nc, mxREAL);
+
+  /* Get input and output pointers. */
+  in  = mxGetPr(prhs[0]);
+  out = mxGetPr(plhs[0]);
+
+  mexcumsum(nr*nc, in, out);
+  
+  return;
+}
