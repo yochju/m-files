fc153d95454f8b28880cfe328c96775885d81d96
Laurent Hoeltgen <hoeltgen@b-tu.de>
2016-05-27 12:33:56 -0600

    reformat

--- mex/f2mex.h
+++ mex/f2mex.h
@@ -8,14 +8,20 @@ extern "C"
 {
 #endif
 
-  extern void mexcumsum (int64_t, double *, double *);
+extern void mexcumsum (int64_t, double *, double *);
 
-  extern void mexstencillocs (int64_t, int64_t, int64_t *, int64_t *, int64_t *);
-  extern void mexstencilmask (int64_t, int64_t, int64_t *, int64_t *, int64_t, int64_t *);
-  extern void mexcreate_5p_stencil (int64_t, int64_t *);
-  extern void mexstencil2sparse_size (int64_t, int64_t *, int64_t *, int64_t *, int64_t *);
-  extern void mexconst_stencil2sparse (int64_t, int64_t *, int64_t *, int64_t *, double *, int64_t *, int64_t *, double *);
-  extern void mexstencil2sparse (int64_t, int64_t *, int64_t *, int64_t *, double *, int64_t *, int64_t *, double *);
+extern void mexstencillocs (int64_t, int64_t, int64_t *, int64_t *, int64_t *);
+
+extern void mexstencilmask (int64_t, int64_t, int64_t *, int64_t *, int64_t, int64_t *);
+
+extern void mexcreate_5p_stencil (int64_t, int64_t *);
+
+extern void mexstencil2sparse_size (int64_t, int64_t *, int64_t *, int64_t *, int64_t *);
+
+extern void mexconst_stencil2sparse (int64_t, int64_t *, int64_t *, int64_t *, double *, int64_t *, int64_t *,
+                                     double *);
+
+extern void mexstencil2sparse (int64_t, int64_t *, int64_t *, int64_t *, double *, int64_t *, int64_t *, double *);
 
 #ifdef __cplusplus
 }
