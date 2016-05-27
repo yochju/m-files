97ed958cf07e0c1a710c802d852ab2e42c75b0e3
Laurent Hoeltgen <hoeltgen@b-tu.de>
2016-05-27 11:56:47 -0600

    completed interface to mod_stencil

--- mex/f2mex.h
+++ mex/f2mex.h
@@ -13,6 +13,9 @@ extern "C"
   extern void mexstencillocs (int64_t, int64_t, int64_t *, int64_t *, int64_t *);
   extern void mexstencilmask (int64_t, int64_t, int64_t *, int64_t *, int64_t, int64_t *);
   extern void mexcreate_5p_stencil (int64_t, int64_t *);
+  extern void mexstencil2sparse_size (int64_t, int64_t *, int64_t *, int64_t *, int64_t *);
+  extern void mexconst_stencil2sparse (int64_t, int64_t *, int64_t *, int64_t *, double *, int64_t *, int64_t *, double *);
+  extern void mexstencil2sparse (int64_t, int64_t *, int64_t *, int64_t *, double *, int64_t *, int64_t *, double *);
 
 #ifdef __cplusplus
 }
